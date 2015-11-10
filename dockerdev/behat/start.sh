8aed8605fae1e609e7cf339ab6a6b07d1c6a515d
Javier Seixas <dev@javierseixas.com>
2015-11-10 07:27:37 +0100

    Extracts debugging conf to docker environment variable

--- dockerdev/behat/start.sh
+++ dockerdev/behat/start.sh
@@ -3,6 +3,5 @@
 echo "xdebug.remote_enable=on" >> /etc/php5/mods-available/xdebug.ini
 echo "xdebug.remote_connect_back=on" >> /etc/php5/mods-available/xdebug.ini
 echo "xdebug.remote_autostart=on" >> /etc/php5/mods-available/xdebug.ini
-echo "xdebug.idekey=PHPSTORM" >> /etc/php5/mods-available/xdebug.ini
 echo "xdebug.remote_port=9000" >> /etc/php5/mods-available/xdebug.ini
 echo "xdebug.remote_host=$(/sbin/ip route|awk '/default/ { print $3 }')" >> /etc/php5/mods-available/xdebug.ini
